c6ff51924e8fc58f0dbef403fdbbd122fabfaba5
suneeti <suneetisharma5591@gmail.com>
2022-06-29 01:04:58 +0530

    Graded AssignmentOOPS

---
+++ src/com/greatlearning/model/AdminDepartment.java
@@ -0,0 +1,18 @@
+package com.greatlearning.model;
+
+public class AdminDepartment extends SuperDepartment {
+    @Override
+    public String departmentName() {
+        return "Admin Deparment";
+    }
+
+    @Override
+    public String getTodaysWork() {
+        return "Complete your documents Submission";
+    }
+
+    @Override
+    public String getWorkDeadline() {
+        return "Complete by EOD";
+    }
+}
---
+++ src/com/greatlearning/model/HRDepartment.java
@@ -0,0 +1,21 @@
+package com.greatlearning.model;
+
+public class HRDepartment extends SuperDepartment{
+    @Override
+    public String departmentName() {
+        return " HR Department ";
+    }
+
+    @Override
+    public String getTodaysWork() {
+        return "Fill today’s worksheet and mark your attendance";
+    }
+
+    @Override
+    public String getWorkDeadline() {
+        return "Complete by EOD";
+    }
+    public String doActivity(){
+        return "team Lunch";
+    }
+}
